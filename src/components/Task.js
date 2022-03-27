af9c76bc1c27d095a17955d54a1b024003d78167
qmkimball <quinnkimball192@gmail.com>
2022-03-27 16:48:14 -0600

    All tasks on sticky notes

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Task = ({ task }) => {
-  return <div>{task.event}</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="#">
+            <h2>TODO</h2>
+            <p>{task.event}</p>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Task;
